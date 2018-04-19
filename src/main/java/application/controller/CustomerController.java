35b0ebc01d61d3ecfffe8c6d0c7a366141b67454
chengyaoZhang <tc2002618@126.com>
2018-04-19 21:02:25 +0800

    controller

---
+++ src/main/java/application/controller/CustomerController.java
@@ -0,0 +1,13 @@
+package application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 管理房客的操作
+ */
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+}
